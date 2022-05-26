6c6bf9897c21498e03adad6b014925080d08ec5c
Dj2035 <dk.jude@yahoo.com>
2022-05-26 07:24:11 +0200

    Update code for appending input value to li

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@ function newItem(){
   //1. Adding a new item to the list of items:
   let li = $('<li></li>');
   let inputValue = $('input').val();
-  li.append(document.createTextNode(inputValue));
+  li.append(inputValue);
 
 
   if (inputValue === '') {
@@ -18,7 +18,7 @@ function newItem(){
 
   //3(i). Adding the delete button "X":
   let crossOutButton = $('<crossOutButton></crossOutButton>');
-  crossOutButton.append(document.createTextNode("X"));
+  crossOutButton.append(document.createTextNode('X'));
   li.append(crossOutButton);
 
   //3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:
